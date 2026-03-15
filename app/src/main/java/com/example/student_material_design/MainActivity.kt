cc126239e8b4714ddb11525765935d841cf92360
VIVIVIciiEND <akirmandinara@gmail.com>
2026-03-15 16:10:31 +0300

    Step 8: Custom shapes added

--- app/src/main/java/com/example/student_material_design/MainActivity.kt
+++ app/src/main/java/com/example/student_material_design/MainActivity.kt
@@ -27,7 +27,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.ui.draw.clip
 import com.example.student_material_design.data.students
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 
 
 class MainActivity : ComponentActivity() {
@@ -53,7 +56,9 @@ fun StudentIcon(
     Image(
         modifier = modifier
             .size(dimensionResource(R.dimen.image_size))
-            .padding(dimensionResource(R.dimen.padding_small)),
+            .padding(dimensionResource(R.dimen.padding_small))
+            .clip(MaterialTheme.shapes.small),
+        contentScale = ContentScale.Crop,
         painter = painterResource(studentIcon),
         contentDescription = null
     )
---
+++ app/src/main/java/com/example/student_material_design/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.example.student_material_design.ui.theme
+
+import android.view.RoundedCorner
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+import com.example.student_material_design.ui.theme.Shapes
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+    medium = RoundedCornerShape(bottomStart = 16.dp , topEnd = 16.dp)
+)
